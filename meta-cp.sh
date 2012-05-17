c0cc119da53ebf74eb45ad6b2e064c3db993437c
Anand Avati <avati@redhat.com>
2012-05-17 12:05:01 -0700

    meta-cp.sh: more space efficient

--- meta-cp.sh
+++ meta-cp.sh
@@ -17,26 +17,101 @@ function header()
     cat > "$dst" <<EOF
 #!/bin/bash -e
 
-function main()
+
+function create_file()
 {
-    if [ \$# -ne 1 ]; then
-        echo "Usage: \$0 <dst-dir>";
-        exit 1;
-    fi
+    local file="\$1";
+    local acc="\$2";
+    local size="\$3";
+    local uid="\$4";
+    local gid="\$5";
 
-    dst="\$1";
-
-    if [ -e "\$dst" -a ! -d "\$dst" ]; then
-        echo "FATAL: \$dst - not a directory";
-        exit 1;
-    fi
-
-    mkdir -p "\$dst";
-
-    cd "\$dst";
-
-    dump;
+    touch "\$file";
+    chmod "\$acc" "\$file";
+    truncate -s "\$size" "\$file";
+    chown -h "\$uid":"\$gid" "\$file";
 }
+
+
+function create_dir()
+{
+    local file="\$1";
+    local acc="\$2";
+    local uid="\$3";
+    local gid="\$4";
+
+    mkdir -p -m "\$acc" "\$file";
+    chown -h "\$uid":"\$gid" "\$file";
+}
+
+
+function create_symlink()
+{
+    local file="\$1";
+    local link="\$2";
+    local uid="\$3";
+    local gid="\$4";
+
+    ln -s "\$link" "\$file";
+    chown -h "\$uid":"\$gid" "\$file";
+}
+
+
+function create_fifo()
+{
+    local file="\$1";
+    local acc="\$2";
+    local uid="\$3";
+    local gid="\$4";
+
+    mkfifo -m "\$acc" "\$file";
+    chown -h "\$uid":"\$gid" "\$file";
+}
+
+
+function create_block()
+{
+    local file="\$1";
+    local acc="\$2";
+    local maj="\$3";
+    local min="\$4";
+    local uid="\$5";
+    local gid="\$6";
+
+    mknod -m "\$acc" "\$file" b "\$maj" "\$min";
+    chown -h "\$uid":"\$gid" "\$file";
+}
+
+
+function create_block()
+{
+    local file="\$1";
+    local acc="\$2";
+    local maj="\$3";
+    local min="\$4";
+    local uid="\$5";
+    local gid="\$6";
+
+    mknod -m "\$acc" "\$file" c "\$maj" "\$min";
+    chown -h "\$uid":"\$gid" "\$file";
+}
+
+
+if [ \$# -ne 1 ]; then
+    echo "Usage: \$0 <dst-dir>";
+    exit 1;
+fi
+
+dst="\$1";
+
+if [ -e "\$dst" -a ! -d "\$dst" ]; then
+    echo "FATAL: \$dst - not a directory";
+    exit 1;
+fi
+
+mkdir -p "\$dst";
+
+cd "\$dst";
 
 EOF
 }
@@ -51,10 +126,6 @@ function body()
 
     cd $src;
 
-cat >> "$dst" <<EOF
-function dump()
-{
-EOF
     find . | while read line; do
 	stats=$(stat -c '"%F" %u %g %a %s %t %T' "$line");
 	eval "set $stats";
@@ -67,62 +138,46 @@ EOF
 	min="$7";
 	link="$8";
 
-	cat >> "$dst" <<EOF
-
-    # name="$line",type="$type",uid="$uid",gid="$gid",acc="$acc",size="$size",maj="$maj",min="$min"
-EOF
-	case "$1" in
+	case "$type" in
 	    'regular file'|'regular empty file')
 		cat >> "$dst" <<EOF
-    \$E touch "$line";
-    \$E chmod "$acc" "$line";
-    \$E truncate -s "$size" "$line";
+create_file "$line" "$acc" "$size" "$uid" "$gid";
 EOF
 		;;
 	    'directory')
 		cat >> "$dst" <<EOF
-    \$E mkdir -p -m "$acc" "$line";
+create_dir "$line" "$acc" "$uid" "$gid";
 EOF
 		;;
 	    'symbolic link')
 		local link=$(readlink "$line");
 		cat >> "$dst" <<EOF
-    \$E ln -s "$link" "$line";
+create_symlink "$line" "$link" "$uid" "$gid";
 EOF
 		;;
 	    'fifo')
 		cat >> "$dst" <<EOF
-    \$E mkfifo -m "$acc" "$line";
+create_fifo "$line" "$acc" "$uid" "$gid";
 EOF
 		;;
 	    'block special file')
 		cat >> "$dst" <<EOF
-    \$E mknod -m "$acc" "$line" b $maj $min;
+create_block "$line" "$acc" "$maj" "$min" "$uid" "$gid";
 EOF
 		;;
 	    'character special file')
 		cat >> "$dst" <<EOF
-    \$E mknod -m "$acc" "$line" c $maj $min;
+create_char "$line" "$acc" "$maj" "$min" "$uid" "$gid";
 EOF
 		;;
 	    *)
 		cat >> "$dst" <<EOF
-    echo Do not know how to recreate "$line"
+echo Do not know how to recreate "$line" of type "$type";
 EOF
 		continue
 		;;
 	esac
-
-	cat >> "$dst" <<EOF
-    \$E chown -h "$uid":"$gid" "$line";
-EOF
     done
-
-cat >> "$dst" <<EOF
-
-}
-
-EOF
 }
 
 
@@ -130,10 +185,6 @@ function footer()
 {
     local src="$1";
     local dst="$2";
-
-    cat >> "$dst" <<EOF
-main "\$@";
-EOF
 }
 
 
